a667ad184f9c458979ecc0be759fb65e96e307ce
Ilya Yatsishin <ilya.yatsishin@jetbrains.com>
2013-06-26 19:19:02 +0400

    WebSocketServlet

--- src/main/java/delightex/server/ChatWebSocketServlet.java
+++ src/main/java/delightex/server/ChatWebSocketServlet.java
@@ -3,11 +3,16 @@ package delightex.server;
 import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocketServlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-public class ChatServlet extends WebSocketServlet {
+public class ChatWebSocketServlet extends WebSocketServlet {
   @Override
   public WebSocket doWebSocketConnect(HttpServletRequest httpServletRequest, String s) {
     return null;
+  }
+
+  @Override
+  public void init() throws ServletException {
   }
 }
