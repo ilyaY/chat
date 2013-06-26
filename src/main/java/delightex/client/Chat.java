e68826c41ee840567c8b8ce7b0ee7acfd8334d5a
Alexander Konstantinov <Alexander.Konstantinov@jetbrains.com>
2013-06-26 18:57:21 +0400

    Init

---
+++ src/main/java/delightex/client/Chat.java
@@ -0,0 +1,11 @@
+package delightex.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class Chat implements EntryPoint {
+  @Override
+  public void onModuleLoad() {
+    Window.alert("Hello!");
+  }
+}
